a29b1b97e3238335b3c104dc3214658218ad35d5
Muhammad Ahsan <ahsan1008@hulhub.com>
2025-09-29 10:24:16 +0500

    GO

---
+++ Projects/SBA.Hierarchy/Controllers/X.Complex/_Grpc_Server/GrpcController.A.Jackson.cs
@@ -0,0 +1,42 @@
+using GLOB.API.Clientz;
+
+using GLOB.Infra.Utils.Attributez;
+using SBA.Projectz.Clientz;
+using SBA.Projectz.Grpc.Service;
+namespace SBA.Auth.Controllers;
+
+public partial class __GrpcServerController : API_1_InjectorController<__GrpcServerController>
+{
+  private readonly GrpcProjectzLookupService grpcProjectzLookupService;
+  // private readonly Projectz_RMQ_Pub _rmqPub;
+  public __GrpcServerController(IServiceProvider sp) : base(sp)
+  {
+    grpcProjectzLookupService = sp.GetSrvc<GrpcProjectzLookupService>();
+    // _rmqPub = sp.GetSrvc<Projectz_RMQ_Pub>();
+  }
+
+  [HttpPost] [NoCache]
+  public async Task<IActionResult> Add([FromBody] ProjectzLookupDtoCreate model)
+  {
+    try
+    {
+      var data = new
+      {
+        model.Name,
+        model.Code,
+        model.Desc,
+        model.ProjectzLookupBaseId,
+        Status.Active,
+        Event = $"ProjectzLookup_{EP.Add}"
+      };
+      MsgBusJackson.Publish(data);
+      return data.ToExtVMSingle().Ok();
+    }
+    catch (Exception ex)
+    {
+      // return ex.Ok();
+      return $"[Rabbit MQ] Error : {ex.Message}".ToExtVMSingle().Ok();
+    }
+
+  }
+}
---
+++ Projects/SBA.Hierarchy/Controllers/X.Complex/_Grpc_Server/GrpcController.CUDS.cs
@@ -0,0 +1,98 @@
+// using RabbitMQ.Client;
+
+// using GLOB.Infra.Utils.Attributez;
+// namespace SBA.Auth.Controllers;
+
+// public partial class __GrpcServerController 
+// {
+
+//   [HttpPost] [NoCache]
+//   public async Task<IActionResult> Createz([FromBody] ProjectzLookupDtoCreate dto)
+//   {
+//     // dto.Status = Status.Active;
+//     try
+//     {
+//       _rmqPub.Publish(dto, (channel, bytes) =>
+//       {
+//        channel.BasicPublish(
+//         exchange: "sba.topic",
+//         routingKey: "auth.lookup.create",
+//         basicProperties: null,
+//         body: bytes
+//       );
+//       });
+//       return dto.ToExtVMSingle().Ok();
+//     }
+//     catch (Exception ex)
+//     {
+//       // return ex.Ok();
+//       return $"[Rabbit MQ] Error : {ex.Message}".ToExtVMSingle().Ok();
+//     }
+
+//   }
+//   [HttpPut("{Id}")] [NoCache]
+//   public async Task<IActionResult> Update(string Id, [FromBody] ProjectzLookupDtoCreate dto)
+//   {
+//     try
+//     {
+//       var newType = dto.ToExpando(
+//         ("Id", Id),
+//         ("Key", "Value")
+//       );
+//       _rmqPub.Publish(newType, (channel, bytes) =>
+//       {
+//        channel.BasicPublish("sba.topic", "auth.lookup.update", null, bytes);
+//       });
+//       return dto.ToExtVMSingle().Ok();
+//     }
+//     catch (Exception ex)
+//     {
+//       // return ex.Ok();
+//       return $"[Rabbit MQ] Error : {ex.Message}".ToExtVMSingle().Ok();
+//     }
+    
+//   }
+
+//   [HttpDelete("{Id}")] [NoCache]
+//   public async Task<IActionResult> Delete(string Id)
+//   {
+//     try
+//     {
+//       DtoRequestDelete<string> dto = new ()
+//       {
+//         Id = Id
+//       };
+//       _rmqPub.Publish(dto, (channel, bytes) =>
+//       {
+//        channel.BasicPublish("sba.topic", "auth.lookup.delete", null, bytes);
+//       });
+//       return $"Deleted Successfully".ToExtVMSingle().Ok();
+//     }
+//     catch (Exception ex)
+//     {
+//       // return ex.Ok();
+//       return $"[Rabbit MQ] Error : {ex.Message}".ToExtVMSingle().Ok();
+//     }
+    
+//   }
+  
+//   [HttpPatch("{Id}")] [NoCache]
+//   public async Task<IActionResult> UpdateStatus(string Id, [FromBody] DtoRequestStatus<string> dto)
+//   {
+//     try
+//     {
+//       dto.Id = Id;
+//       _rmqPub.Publish(dto, (channel, bytes) =>
+//       {
+//        channel.BasicPublish("sba.topic", "auth.lookup.status", null, bytes);
+//       });
+//       return dto.ToExtVMSingle().Ok();
+//     }
+//     catch (Exception ex)
+//     {
+//       // return ex.Ok();
+//       return $"[Rabbit MQ] Error : {ex.Message}".ToExtVMSingle().Ok();
+//     }
+    
+//   }
+// }
